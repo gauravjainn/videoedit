0c867f231b9d9d12313907e15bd9c4304118faef
gauravjainn <itsgauravjain.gj@gmail.com>
2021-05-19 22:03:38 +0530

    add lib

--- App.js
+++ App.js
@@ -7,90 +7,155 @@
  */
 
 import React from 'react';
-import type {Node} from 'react';
 import {
   SafeAreaView,
-  ScrollView,
-  StatusBar,
   StyleSheet,
-  Text,
-  useColorScheme,
+  ScrollView,
   View,
+  Text,
+  StatusBar,
+  TouchableHighlight,
 } from 'react-native';
 
 import {
-  Colors,
-  DebugInstructions,
   Header,
   LearnMoreLinks,
+  Colors,
+  DebugInstructions,
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
+import {VESDK, Configuration, TintMode} from 'react-native-videoeditorsdk';
 
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
+/**
+ * Uncomment the following single line of code to unlock VideoEditor SDK automatically
+ * for both platforms. Every platform requires a separate license file which must be
+ * named `vesdk_license.ios.json` for the iOS license and `vesdk_license.android.json`
+ * for the Android license file.
+ */
+// VESDK.unlockWithLicense(require('./vesdk_license'));
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+const App: () => React$Node = () => {
+  const openEditor = () => {
+    // Set up sample video
+    let video = require('./assets/Skater.mp4');
+    // Set up configuration
+    let configuration: Configuration = {
+      // Configure sticker tool
+      sticker: {
+        // Enable personal stickers
+        personalStickers: true,
+        // Configure stickers
+        categories: [
+          // Create sticker category with stickers
+          {
+            identifier: 'example_sticker_category_logos',
+            name: 'Logos',
+            thumbnailURI: require('./assets/React-Logo.png'),
+            items: [
+              {
+                identifier: 'example_sticker_logos_react',
+                name: 'React',
+                stickerURI: require('./assets/React-Logo.png'),
+              },
+              {
+                identifier: 'example_sticker_logos_imgly',
+                name: 'img.ly',
+                stickerURI: require('./assets/imgly-Logo.png'),
+                tintMode: TintMode.SOLID,
+              },
+            ],
+          },
+          // Reorder and use existing sticker categories
+          {identifier: 'imgly_sticker_category_animated'},
+          {identifier: 'imgly_sticker_category_emoticons'},
+          // Modify existing sticker category
+          {
+            identifier: 'imgly_sticker_category_shapes',
+            items: [
+              {identifier: 'imgly_sticker_shapes_badge_01'},
+              {identifier: 'imgly_sticker_shapes_arrow_02'},
+              {identifier: 'imgly_sticker_shapes_spray_03'},
+            ],
+          },
+        ],
+      },
+    };
+    VESDK.openEditor(video, configuration).then(
+      (result) => {
+        console.log(result);
+      },
+      (error) => {
+        console.log(error);
+      },
+    );
   };
-
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+    <>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView>
+        <ScrollView
+          contentInsetAdjustmentBehavior="automatic"
+          style={styles.scrollView}>
+          <Header />
+          {global.HermesInternal == null ? null : (
+            <View style={styles.engine}>
+              <Text style={styles.footer}>Engine: Hermes</Text>
+            </View>
+          )}
+          <View style={styles.body}>
+            <View style={styles.sectionContainer}>
+              <Text style={styles.sectionTitle}>VideoEditor SDK</Text>
+              <TouchableHighlight onPress={openEditor}>
+                <Text style={styles.sectionDescription}>
+                  Click here to <Text style={styles.highlight}>edit a sample video</Text>.
+                </Text>
+              </TouchableHighlight>
+            </View>
+            <View style={styles.sectionContainer}>
+              <Text style={styles.sectionTitle}>Step One</Text>
+              <Text style={styles.sectionDescription}>
+                Edit <Text style={styles.highlight}>App.js</Text> to change this
+                screen and then come back to see your edits.
+              </Text>
+            </View>
+            <View style={styles.sectionContainer}>
+              <Text style={styles.sectionTitle}>See Your Changes</Text>
+              <Text style={styles.sectionDescription}>
+                <ReloadInstructions />
+              </Text>
+            </View>
+            <View style={styles.sectionContainer}>
+              <Text style={styles.sectionTitle}>Debug</Text>
+              <Text style={styles.sectionDescription}>
+                <DebugInstructions />
+              </Text>
+            </View>
+            <View style={styles.sectionContainer}>
+              <Text style={styles.sectionTitle}>Learn More</Text>
+              <Text style={styles.sectionDescription}>
+                Read the docs to discover what to do next:
+              </Text>
+            </View>
+            <LearnMoreLinks />
+          </View>
+        </ScrollView>
+      </SafeAreaView>
+    </>
   );
 };
 
 const styles = StyleSheet.create({
+  scrollView: {
+    backgroundColor: Colors.lighter,
+  },
+  engine: {
+    position: 'absolute',
+    right: 0,
+  },
+  body: {
+    backgroundColor: Colors.white,
+  },
   sectionContainer: {
     marginTop: 32,
     paddingHorizontal: 24,
@@ -98,14 +163,24 @@ const styles = StyleSheet.create({
   sectionTitle: {
     fontSize: 24,
     fontWeight: '600',
+    color: Colors.black,
   },
   sectionDescription: {
     marginTop: 8,
     fontSize: 18,
     fontWeight: '400',
+    color: Colors.dark,
   },
   highlight: {
     fontWeight: '700',
+  },
+  footer: {
+    color: Colors.dark,
+    fontSize: 12,
+    fontWeight: '600',
+    padding: 4,
+    paddingRight: 12,
+    textAlign: 'right',
   },
 });
 
---
+++ README.md
@@ -0,0 +1,58 @@
+<p align="center">
+  <a href="https://www.videoeditorsdk.com/?utm_campaign=Projects&utm_source=Github&utm_medium=VESDK&utm_content=React-Native-Demo">
+    <img src="https://static.photoeditorsdk.com/vesdk/vesdk-logo-s.svg" alt="VideoEditor SDK Logo"/>
+  </a>
+</p>
+<p align="center">
+  <a href="https://npmjs.org/package/react-native-videoeditorsdk">
+    <img src="https://img.shields.io/npm/v/react-native-videoeditorsdk.svg" alt="NPM version">
+  </a>
+  <a href="https://npmjs.org/package/react-native-videoeditorsdk">
+    <img src="https://img.shields.io/badge/platforms-android%20|%20ios-lightgrey.svg" alt="Platform support">
+  </a>
+  <a href="http://twitter.com/VideoEditorSDK">
+    <img src="https://img.shields.io/badge/twitter-@VideoEditorSDK-blue.svg?style=flat" alt="Twitter">
+  </a>
+</p>
+
+# VideoEditor SDK React Native Example App
+
+This project shows how to integrate [VideoEditor SDK](https://www.videoeditorsdk.com/?utm_campaign=Projects&utm_source=Github&utm_medium=VESDK&utm_content=React-Native-Demo) into a React Native application with the [React Native module for VideoEditor SDK](https://github.com/imgly/vesdk-react-native) which is available via NPM as [`react-native-videoeditorsdk`](https://www.npmjs.com/package/react-native-videoeditorsdk).
+
+## Getting started
+
+After cloning this repository, perform the following steps to run the example application:
+
+```sh
+# install
+yarn install
+cd ios && pod install && cd .. # CocoaPods on iOS needs this extra step
+# run
+yarn react-native run-ios
+# or
+yarn react-native run-android
+```
+
+## Unlock the SDK
+
+VideoEditor SDK is a product of img.ly GmbH. Without unlocking, the SDK is fully functional but a watermark is added on top of the video preview and any exported videos.
+In order to remove the watermark and to use VideoEditor SDK within your app **you'll need to [request a license](https://account.photoeditorsdk.com/pricing?product=vesdk&?utm_campaign=Projects&utm_source=Github&utm_medium=VESDK&utm_content=React-Native-Demo) for each platform and load the license file(s)** in your [`App.js`](./App.js#L30-L36) with the following single line of code that automatically resolves multiple license files via [platform-specific file extensions](https://reactnative.dev/docs/platform-specific-code#platform-specific-extensions):
+
+```js
+VESDK.unlockWithLicense(require('./vesdk_license'));
+```
+
+## VideoEditor SDK for iOS & Android
+
+The React Native module for VideoEditor SDK includes a rich set of most commonly used [configuration and customization options](https://github.com/imgly/vesdk-react-native/blob/master/configuration.ts) of VideoEditor SDK for iOS and Android. The native frameworks provide **fully customizable** video editors. Please refer to [our documentation](https://docs.videoeditorsdk.com?utm_campaign=Projects&utm_source=Github&utm_medium=VESDK&utm_content=React-Native-Demo) for more details.
+
+Native customization for iOS is demonstrated in the [`AppDelegate`](./ios/VESDKExample/AppDelegate.m#L36-L47) of the example application.
+
+## License Terms
+
+Make sure you have a [commercial license](https://account.photoeditorsdk.com/pricing?product=vesdk&?utm_campaign=Projects&utm_source=Github&utm_medium=VESDK&utm_content=React-Native-Demo) for VideoEditor SDK before releasing your app.
+A commercial license is required for any app or service that has any form of monetization: This includes free apps with in-app purchases or ad supported applications. Please contact us if you want to purchase the commercial license.
+
+## Support and License
+
+Use our [service desk](http://support.videoeditorsdk.com) for bug reports or support requests. To request a commercial license, please use the [license request form](https://account.photoeditorsdk.com/pricing?product=vesdk&?utm_campaign=Projects&utm_source=Github&utm_medium=VESDK&utm_content=React-Native-Demo) on our website.
